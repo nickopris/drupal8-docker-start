77fdb00e630b41a91bf42e2ce63a16c5c6919f9a
Nick Opris <oprisnicolae@gmail.com>
2018-02-02 16:26:56 +0000

    files

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+timeout () {
+    tput sc
+    time=$1; while [ $time -ge 0 ]; do
+        tput rc; tput el
+        printf "$2" $time
+        ((time--))
+        sleep 1
+    done
+    tput rc; tput ed;
+}
+
+docker-sync start
+docker-compose up -d
+timeout 20 "Waiting %s seconds for files to be copied across..."
+echo ""
+echo "Your development website is up and running."
+echo ""
+echo "Check this link for all endpoints: https://github.com/wodby/docker4drupal/blob/master/docs/domains.md"
+open http://portainer.drupal.docker.localhost:8000/#/containers/
+open http://drupal.docker.localhost:8000
+echo "Done"
+
---
+++ stop.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker-sync stop
+docker-compose down
